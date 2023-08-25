1ab69e39298312bfe1746a074db9caadf653fff8
THEEKSHANA88 <thenuwan.20200911@iit.ac.lk>
2023-08-25 12:18:11 +0530

    project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+type Todo = {
+  id: number;
+  text: string;
+};
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  const handleAddTodo = () => {
+    if (newTodo.trim() === "") return;
+    const newTodoItem: Todo = {
+      id: Date.now(),
+      text: newTodo,
+    };
+    setTodos([...todos, newTodoItem]);
+    setNewTodo("");
+  };
+
+  const handleDeleteTodo = (id: number) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-4">
+      <h1>To-Do App</h1>
+      <div className="input-group">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Add a new to-do..."
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button className="btn btn-primary" onClick={handleAddTodo}>
+          Add
+        </button>
+      </div>
+      <ul className="list-group">
+        {todos.map((todo) => (
+          <li className="list-group-item d-flex justify-content-between" key={todo.id}>
+            {todo.text}
+            <button className="btn btn-danger btn-sm" onClick={() => handleDeleteTodo(todo.id)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
